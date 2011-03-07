8dd9f5e7841aa2d3a8922f3f8346be9a8a78d1ad
Jaap de Haan <jaap.dehaan@color-of-code.de>
2011-03-07 14:44:44 +0100

    Added a project that converts C code to C# code using stdc

---
+++ Csharpenizer/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Csharpenizer {
+	class Program {
+		static void Main (string[] args)
+		{
+		}
+	}
+}
