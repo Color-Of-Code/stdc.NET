7c32a355415f4bb3e27298f7bb41fa5b6a424e0f
Jaap de Haan <jaap.dehaan@alpine.de>
2019-02-26 21:46:45 +0100

    Replacement for embedded resources in .NET core

--- gplex/SpecFiles/GenerateAll.sh
+++ gplex/SpecFiles/GenerateAll.sh
@@ -1,22 +1,12 @@
 #!/bin/bash
 
-# TODO: find a replacement for .NET core
-# generate resource resX file and copy to destination
-#csc GenerateResource.cs
-#GenerateResource.exe
-#move Content.resx ..\IncludeResources
-#del GenerateResource.exe
-
 # generate a fresh copy of Parser.cs
 cd gppg
 dotnet run /gplex /nolines ../gplex/SpecFiles/gplex.y
 mv Parser.cs ../gplex/GPLEX
 
 # generate a fresh copy of Scanner.cs
-gplex gplex.lex
+cd gplex
+dotnet run SpecFiles/gplex.lex
 mv Scanner.cs ../gplex/GPLEX
-
-if not exist GplexBuffers.cs goto finish
 mv GplexBuffers.cs ../gplex/GPLEX
-
-
