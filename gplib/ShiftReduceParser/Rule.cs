ba69b7802d63c3c33a89a1016dc7f648579943c8
Jaap de Haan <ColorOfCode@googlemail.com>
2019-03-09 21:33:46 +0100

    Split classes in their own files

---
+++ gplib/ShiftReduceParser/Rule.cs
@@ -0,0 +1,29 @@
+// Gardens Point Parser Generator
+// Copyright (c) Wayne Kelly, QUT 2005-2014
+// (see accompanying GPPGcopyright.rtf)
+
+
+namespace QUT.Gplib
+{
+    /// <summary>
+    /// Rule representation at runtime.
+    /// </summary>
+    public class Rule
+    {
+        internal int LeftHandSide; // symbol
+        internal int[] RightHandSide; // symbols
+
+        /// <summary>
+        /// Rule constructor.  This holds the ordinal of
+        /// the left hand side symbol, and the list of
+        /// right hand side symbols, in lexical order.
+        /// </summary>
+        /// <param name="left">The LHS non-terminal</param>
+        /// <param name="right">The RHS symbols, in lexical order</param>
+        public Rule(int left, int[] right)
+        {
+            this.LeftHandSide = left;
+            this.RightHandSide = right;
+        }
+    }
+}
